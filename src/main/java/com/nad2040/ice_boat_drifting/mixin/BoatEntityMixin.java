e858197e16a6940d64c59e429b7a5f56cb3b8017
nad <58147198+nad2040@users.noreply.github.com>
2023-06-21 09:56:44 +0800

    v1

---
+++ src/main/java/com/nad2040/ice_boat_drifting/mixin/BoatEntityMixin.java
@@ -0,0 +1,24 @@
+package com.nad2040.ice_boat_drifting.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BoatEntity.class)
+public abstract class BoatEntityMixin {
+	@Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V")
+	public void BoatEntity(EntityType<? extends BoatEntity> entityType, World world, CallbackInfo ci) {
+		((BoatEntity) (Object) this).setStepHeight(1.0f);
+	}
+
+	@ModifyConstant(constant = @Constant(floatValue = 0.9F, ordinal = 2), method = "updateVelocity()V")
+	public float updateVelocity(float f) {
+		return 0.99F;
+	}
+}
